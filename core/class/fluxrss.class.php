0ac92f9a8b81cd06eabf15c2dc29fe0a9c8fcfb8
Cédric Dupont <lunarok@gmail.com>
2017-02-07 14:58:27 +0100

    UTF

--- core/class/fluxrss.class.php
+++ core/class/fluxrss.class.php
@@ -45,7 +45,7 @@ class fluxrss extends eqLogic {
 
     public function updateRss($item) {
 
-        $rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>';
+        $rssfeed = '<?xml version="1.0" encoding="UTF-8"?>';
         $rssfeed .= '<rss version="2.0">';
         $rssfeed .= '<channel>';
         $rssfeed .= '<title>' . $this->getConfiguration('title') . '</title>';
