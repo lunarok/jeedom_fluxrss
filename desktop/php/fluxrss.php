ba85673974ccc967db5faf3d0cd41fd6380a90d8
Ethal <developer@ethal-consulting.com>
2018-06-07 12:05:11 +0200

    Update fluxrss.php
    Bonjour,
    il m'arrive de ne pas avoir accès à mon jeedom (ie déplacement,....) possible de rajouter cette option
    Merci

--- desktop/php/fluxrss.php
+++ desktop/php/fluxrss.php
@@ -137,6 +137,7 @@ $eqLogics = eqLogic::byType('fluxrss');
 									<option value="20">20</option>
 									<option value="25">25</option>
 									<option value="30">30</option>
+									<option value="50">50</option>
 								</select>
 							</div>
 						</div>
